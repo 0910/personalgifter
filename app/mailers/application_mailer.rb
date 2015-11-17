80e68cb01f71eb3f1cb94f17cce1ea0c203cdde2
0910 <e@nuevediez.com.ar>
2015-11-17 13:27:09 -0300

    User mailer redefined

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
-class UserMailer < ApplicationMailer
-  default from: 'info@personalgifter.com'
- 
-  def deliver_welcome_email(user)
-    @user = user
-    @url  = 'http://personalgifter.com/login'
-    mail(to: @user.email, subject: 'Bienvenido a Personal Gifter')
-  end
+class ApplicationMailer < ActionMailer::Base
+  default from: "from@example.com"
+  layout 'mailer'
 end
