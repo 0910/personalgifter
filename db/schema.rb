fdd303b50b86589a5150f769d2bb12ca50df4885
0910 <e@nuevediez.com.ar>
2015-12-03 15:05:32 -0300

    db schema fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203161729) do
+ActiveRecord::Schema.define(version: 20151202190227) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -152,7 +152,6 @@ ActiveRecord::Schema.define(version: 20151203161729) do
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
     t.boolean  "cover",             limit: 1,   default: false
-    t.integer  "group_id",          limit: 4
   end
 
   add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
@@ -197,9 +196,8 @@ ActiveRecord::Schema.define(version: 20151203161729) do
 
   create_table "occasions", force: :cascade do |t|
     t.string   "name",       limit: 255
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "available",  limit: 255, default: "Yes"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "product_groups", force: :cascade do |t|
@@ -228,7 +226,6 @@ ActiveRecord::Schema.define(version: 20151203161729) do
     t.integer  "hits_count",      limit: 4,                    default: 0,     null: false
     t.integer  "category_id",     limit: 4
     t.string   "available",       limit: 255,                  default: "Yes"
-    t.string   "public_price",    limit: 255
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
