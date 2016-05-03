6924a679d9264f8849e65705b940a09a5b4d66be
0910 <e@nuevediez.com.ar>
2016-05-03 12:58:06 -0300

    debuggin stores validation passed

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   belongs_to :user
   has_many :products, :dependent => :nullify
-  validates :name, uniqueness: { case_sensitive: true }
+  validates :name, uniqueness: { case_sensitive: false }
 
   def self.options_for_select
 	order('LOWER(name)').map { |e| [e.name, e.id] }
